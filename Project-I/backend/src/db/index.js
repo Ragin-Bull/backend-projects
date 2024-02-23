91291f6b32af4f221819a0e0395da8fae80558ae
Shatansh <shatansh.iitkgp@gmail.com>
2024-02-23 18:57:00 +0530

    Fixed bugs

--- Project-I/backend/src/db/index.js
+++ Project-I/backend/src/db/index.js
@@ -4,7 +4,7 @@ import { DB_NAME } from "../constants.js";
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(
-      `${process.env.MONGODB_URI}/${DB_NAME}`
+      `${process.env.MONGODB_URIs}/${DB_NAME}`
     );
     console.log(
       `\nMongoDB Connected!! DB HOST: ${connectionInstance.connection.host}`
--- Project-I/backend/src/index.js
+++ Project-I/backend/src/index.js
@@ -1,9 +1,16 @@
-import dotenv from "dotenv";
-import express from "express";
 import connectDB from "./db/index.js";
+import express from "express";
 const app = express();
-dotenv.config({
-  path: "./.env",
-});
-console.log(process.env.MONGODB_URI);
-connectDB();
+
+console.log(process.env.MONGODB_URIs);
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+  });
+
+// Run node --env-file=.env index.js
